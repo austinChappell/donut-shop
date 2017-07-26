7ee82bbaf6dbf424dac5d060f4e6abeef088bfc6
austinChappell <Austin.W.Chappell@gmail.com>
2017-07-26 13:45:40 -0500

    add dropdown menu items

--- index.js
+++ index.js
@@ -30,3 +30,65 @@ $(document).ready(function(){
     $('#bs-example-navbar-collapse-1').removeClass('in');
   });
 });
+
+let menuSubTitle = document.querySelectorAll('.menu-sub-title');
+let menuSection = document.querySelector('#menu-section');
+
+menuSection.addEventListener('click', function(e) {
+
+  let downIcon = document.querySelector('.fa-caret-down');
+  let subTitle = e.target;
+  function findSubTitle() {
+    if (!subTitle.classList.contains('menu-sub-title')) {
+      subTitle = subTitle.parentNode;
+      findSubTitle();
+    };
+  };
+
+  findSubTitle();
+
+  if (downIcon) {
+    downIcon.classList.remove('fa-caret-down');
+    downIcon.classList.add('fa-caret-right');
+  }
+
+  let icon = subTitle.children[0].children[0];
+  let subSubTitles = document.querySelectorAll('.meat-choices');
+  let children = subTitle.children[0].children;
+  let ul = subTitle.nextElementSibling;
+
+  // IF THE ITEM IS CLOSED
+  if (ul.classList.contains('hide')) {
+    subSubTitles.forEach(function(item) {
+      item.classList.add('hide');
+    });
+    if (children.length > 2) {
+      children[2].classList.remove('hide');
+    };
+    icon.classList.remove('fa-caret-right');
+    icon.classList.add('fa-caret-down');
+    menuSubTitle.forEach(function(item) {
+      item.nextElementSibling.classList.add('hide');
+    });
+    ul.classList.remove('hide');
+
+  // IF THE ITEM IS OPENED
+  } else {
+    subSubTitles.forEach(function(item) {
+      item.classList.add('hide');
+    });
+    ul.classList.add('hide');
+  };
+
+});
+
+
+// menuSubTitle.addEventListener('click', function() {
+//   alert('hi');
+//   // if (this.nextElementSibling.classList.contains('hide')) {
+//   //   menuSubTitle.nextElementSibling.classList.add('hide');
+//   //   this.nextElementSibling.classList.remove('hide');
+//   // } else {
+//   //   this.nextElementSibling.classList.add('hide');
+//   // };
+// });
